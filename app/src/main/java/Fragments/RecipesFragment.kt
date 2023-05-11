eb23b3b5e8b8d70961bb0dba54ccc8ccc3984feb
aguscaffidi <aguscaffidi@gmail.com>
2023-05-11 14:14:19 -0300

    1

---
+++ app/src/main/java/Fragments/RecipesFragment.kt
@@ -0,0 +1,32 @@
+package Fragments
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.recycleview.R
+
+class RecipesFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = RecipesFragment()
+    }
+
+    private lateinit var viewModel: RecipesViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_recipes, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(RecipesViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/Fragments/RecipesViewModel.kt
@@ -0,0 +1,7 @@
+package Fragments
+
+import androidx.lifecycle.ViewModel
+
+class RecipesViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
